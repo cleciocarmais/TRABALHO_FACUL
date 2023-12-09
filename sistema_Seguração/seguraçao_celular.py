1b4a5ac194342bf1dcc71b406c962fff7ea6ed1a
cleciocarmais <franciscoclecioti@gmail.com>
2023-12-09 11:03:09 -0300

    atua

--- sistema_Seguração/seguraçao_celular.py
+++ sistema_Seguração/seguraçao_celular.py
@@ -15,16 +15,22 @@ class Segurança:
     def verificar_senha(self):
         self.verificar = True
         while self.verificar:
+            print("ENTRADA INICIAL")
+            print(f'descrição do {self.verificar}')
             self.senha = input("Digite uma Senha: ").upper()
             self.senha2 = input('Confirme a senha: ').upper()
 
             if self.senha == self.senha2 and self.senha:
                 print("Senha salva com sucesso.")
+
                 self.verificar = False
             else:
                 print('Senha incorreta ou vazia. Tente novamente.')
+        print(f'descrição tipo dois {self.verificar}')
+        
 
         self.executar_acao()
+        print("PASSOU AQUI DA EXECUÇÃO")
 
     def executar_acao(self):
         self.pergunta = input('''Execute a ação:\n
@@ -32,15 +38,21 @@ class Segurança:
               2 - Desbloquer Celular
               3 - Desligar Celular
               4 - Desligar Dados moveis
-            
+            1
               ''')
 
         if self.pergunta == '1':
             self.bloqueia_celular()
+            print("ACESSO BLOQUEIADO")
         elif self.pergunta == '2':
-            self.desbloquea()
+            self.desbloqueia_celular()
+            print("DESBLOQUEIADO")
         elif self.pergunta == '3':
-            self.desligar()
+            self.desligar_celular()
+            print('DESLIGADO CELULAR')
+        elif self.pergunta == '4':
+            self.desligar_dados_moveis()
+            print('DESLIGADO DADOS MOVEIS')
     
 
     def bloqueia_celular(self):
@@ -52,32 +64,47 @@ class Segurança:
             print('Celular já está bloqueado.')
             self.executar_acao()
 
-    def desbloquea(self):
-        if self.status != self.UNLOCKED:
+    def desbloqueia_celular(self):
+        if self.status == self.LOCKED:
             SENHA = input('Digite a senha')
             if SENHA == self.senha:
                 self.status = self.UNLOCKED
                 print("Celular desbloqueado")
-                self.executar_acao()
+                # self.executar_acao()
             else:
                 print('Senha incorreta')
                 self.executar_acao()
         else:
-            print("CELULAR DESBLOQUEIDO")
+            print("Celular ja desbloqueiado")
             self.executar_acao()
     
 
-    def desligar(self):
-        if self.celular != self.OFF:
-            senha = input('Digite a senha ')
-            if self.senha == senha:
-                self.celular = self.OFF
-                print("celular desligado")
+    def desligar_celular(self):
+        if self.status == self.LOCKED:
+            if self.celular != self.OFF:
+                senha = input('Digite a senha ')
+                if self.senha == senha:
+                    self.celular = self.OFF
+                    print("celular desligado")
 
-            else:
-                print("Senha Incorreta")
-                self.executar_acao()
-       
+                else:
+                    print("Senha Incorreta")
+                    self.executar_acao()
+        else:
+            print('Celular desligado')
+    def desligar_dados_moveis(self):
+        if self.status == self.LOCKED:
+            if self.dados_moveis != self.OFF:
+                senha = input("Digite sua Senha")
+                if senha == self.senha:
+                    print('Dados moveis Desligado')
+                    self.executar_acao()
+                else:
+                    print('Senha Incorreta')
+                    self.executar_acao()
+        else:
+            print('Dados Moveis Desligados')
+            self.executar_acao()
 
                 
             
@@ -85,10 +112,3 @@ class Segurança:
 # Example usage:
 seguranca = Segurança()
 
-        
-
-    
-        
-  
-celular = Segurança()
-print(celular.executar_acao)
